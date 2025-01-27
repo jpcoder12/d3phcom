74491b310edfda5e6ce68480b7477b1d0df042fd
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2025-01-27 19:48:05 +0000

    fixed responsive issue

--- app/components/tweets/components/TweetCard.tsx
+++ app/components/tweets/components/TweetCard.tsx
@@ -14,7 +14,7 @@ export function TweetCard({ tweet }: TweetCardProps) {
   };
   return (
     <Link href={`https://x.com/MacRumors/status/${tweet.tweet_id}`} passHref target='_blank'>
-      <Card className=' xs:w-[400px] xs:h-[200px]  sm:w-[500px] md:w-[600px] lg:w-[300px] xl:w-[400px] xl:h-[280px]  4xl:h-[290px] 4xl:w-[780px] flex flex-col bg-card border border-card-border hover:bg-black rounded-lg'>
+      <Card className=' xs:w-[400px] xs:h-[200px]  sm:w-[500px] md:w-[500px] lg:w-[300px] xl:w-[400px] xl:h-[280px] 4xl:h-[290px] 4xl:w-[780px] flex flex-col bg-card border border-card-border hover:bg-black rounded-lg'>
         <CardContent className='flex-grow pt-4 text-gray-400'>
           <p className='text-base'>{truncateText(tweet.text, 100)}</p>
         </CardContent>
