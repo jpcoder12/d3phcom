b4d76b40631482ec077dfdb00660744a9c3407ec
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2025-01-27 18:29:12 +0000

    created dynamic link to tweets by tweet_id

--- app/components/tweets/components/TweetCard.tsx
+++ app/components/tweets/components/TweetCard.tsx
@@ -2,6 +2,7 @@
 
 import { Card, CardContent, CardFooter } from "../../ui/card";
 import { Tweet } from "@/app/types";
+import Link from "next/link";
 
 interface TweetCardProps {
   tweet: Tweet;
@@ -9,14 +10,16 @@ interface TweetCardProps {
 
 export function TweetCard({ tweet }: TweetCardProps) {
   return (
-    <Card className='w-full h-full flex flex-col bg-card border border-card-border  rounded-lg'>
-      <CardContent className='flex-grow pt-4 text-gray-400'>
-        <p className='text-base'>{tweet.text}</p>
-      </CardContent>
-      <CardFooter className='text-sm text-gray-400'>
-        <p>Query: {tweet.query_kw}</p>
-        <p className='ml-auto'>Tweet ID: {tweet.tweet_id}</p>
-      </CardFooter>
-    </Card>
+    <Link href={`https://x.com/MacRumors/status/${tweet.tweet_id}`} passHref target='_blank'>
+      <Card className='w-full h-full flex flex-col bg-card border border-card-border  rounded-lg'>
+        <CardContent className='flex-grow pt-4 text-gray-400'>
+          <p className='text-base'>{tweet.text}</p>
+        </CardContent>
+        <CardFooter className='text-sm text-gray-400'>
+          <p>Query: {tweet.query_kw}</p>
+          <p className='ml-auto'>Tweet ID: {tweet.tweet_id}</p>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
