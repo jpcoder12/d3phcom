fbec8b54540fb69c844aa051850dcf406d302db7
jpcoder12 <117314890+jpcoder12@users.noreply.github.com>
2025-02-14 09:33:49 -0700

    Update input.tsx
    ignore this--small break fix with deployment

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           className
         )}
         ref={ref}
-        {...props}
+        {...props} // 
       />
     )
   }
